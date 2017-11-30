f8766bc061076e706c49f56c116f5d45ac814706
Rainer Sawitzki <training@rainer-sawitzki.de>
2017-11-30 13:47:42 +0100

    add simplerouting using spring xml

---
+++ org.javacream.training.apache.camel/src/main/java/org/javacream/training/apache/camel/simplerouting/SimpleRoutingSpringStarter.java
@@ -0,0 +1,18 @@
+package org.javacream.training.apache.camel.simplerouting;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SimpleRoutingSpringStarter {
+
+	public static void main(String[] args) {
+		new ClassPathXmlApplicationContext("simpleroute-camel-context.xml");
+		Object block = new Object();
+		synchronized (block) {
+			try {
+				block.wait();
+			} catch (Exception e) {
+				// OK
+			}
+		}
+	}
+}
