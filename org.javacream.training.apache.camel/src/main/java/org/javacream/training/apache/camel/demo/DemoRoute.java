333d1721c1c99ff89d79ad1f06c6e067c9edcdaf
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-01-22 09:45:57 +0100

    implement as spring boot application

--- org.javacream.training.apache.camel/src/main/java/org/javacream/training/apache/camel/demo/DemoRoute.java
+++ org.javacream.training.apache.camel/src/main/java/org/javacream/training/apache/camel/demo/DemoRoute.java
@@ -1,14 +1,12 @@
 package org.javacream.training.apache.camel.demo;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DemoRoute extends RouteBuilder {
-
 	@Override
 	public void configure() throws Exception {
 		from("file:data/indir?noop=true").to("file:data/outdir");
 	}
-
-	
-
 }
