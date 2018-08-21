0806f0f55778cd85066aa0a7a564620e1d4e0722
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-08-21 09:45:22 +0100

    add business spring beans

--- org.javacream.training.apache.camel/src/main/java/org/javacream/training/apache/camel/complexfile/ContentProcessor.java
+++ org.javacream.training.apache.camel/src/main/java/org/javacream/training/apache/camel/complexfile/ContentProcessor.java
@@ -4,10 +4,11 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.javacream.training.util.TextTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ContentProcessor implements Processor {
 
-	private TextTransformer transformer;
+	@Autowired private TextTransformer transformer;
 
 	public void setTransformer(TextTransformer transformer) {
 		this.transformer = transformer;
