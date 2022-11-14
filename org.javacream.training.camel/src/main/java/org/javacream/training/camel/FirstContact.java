432357ec872afb32e458a376b28f192fe60f31d6
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-14 12:01:12 +0100

    first contact

---
+++ org.javacream.training.camel/src/main/java/org/javacream/training/camel/FirstContact.java
@@ -0,0 +1,25 @@
+package org.javacream.training.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class FirstContact {
+	public static void main(String[] args) {
+		System.out.println("Starting main...");
+		try {
+			DefaultCamelContext context = new DefaultCamelContext();
+			RouteBuilder builder = new RouteBuilder() {
+
+				@Override
+				public void configure() throws Exception {
+					from("timer:name_of_the_timer_unused_here").log("Hello World!");
+				}
+			};
+			context.addRoutes(builder);
+			context.start();
+			Thread.sleep(5000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
