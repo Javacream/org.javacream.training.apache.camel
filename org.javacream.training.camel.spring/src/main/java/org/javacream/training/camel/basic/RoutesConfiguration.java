18ae0d26ca3609a08d81f6bba3b2071928e8ccbd
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-15 11:20:40 +0100

    finish @Configuration

--- org.javacream.training.camel.spring/src/main/java/org/javacream/training/camel/basic/RoutesConfiguration.java
+++ org.javacream.training.camel.spring/src/main/java/org/javacream/training/camel/basic/RoutesConfiguration.java
@@ -1,6 +1,7 @@
 package org.javacream.training.camel.basic;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.javacream.training.camel.basic.processor.StringConverterProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +10,25 @@ import org.springframework.context.annotation.Configuration;
 public class RoutesConfiguration {
 
 	@Bean
-	public RouteBuilder route1(@Value("${route1.from}") String from, @Value("${route1.to}") String to) {
-		return new BasicRoute(from, to);
+	public RouteBuilder route1(@Value("${route1.from}") String from, @Value("${route1.to}") String to,
+			StringConverterProcessor processor) {
+		return new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from(from).process(processor).to(to);
+			}
+		};
 	}
 
 	@Bean
-	public RouteBuilder route2(@Value("${route2.from}") String from, @Value("${route2.to}") String to) {
-		return new BasicRoute(from, to);
+	public RouteBuilder route2(@Value("${route2.from}") String from, @Value("${route2.to}") String to,
+			StringConverterProcessor processor) {
+		return new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from(from).process(processor).to(to);
+			}
+		};
 	}
 
 }
