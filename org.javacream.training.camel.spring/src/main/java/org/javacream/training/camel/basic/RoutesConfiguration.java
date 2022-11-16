594d396d445915f62d1e8403618ab6a1318d7c83
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-11-16 09:13:28 +0100

    add Rest sample

--- org.javacream.training.camel.spring/src/main/java/org/javacream/training/camel/basic/RoutesConfiguration.java
+++ org.javacream.training.camel.spring/src/main/java/org/javacream/training/camel/basic/RoutesConfiguration.java
@@ -2,7 +2,6 @@ package org.javacream.training.camel.basic;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.javacream.training.camel.basic.processor.StringConverterProcessor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -12,42 +11,14 @@ import org.springframework.context.annotation.ImportResource;
 public class RoutesConfiguration {
 
 	@Bean
-	public RouteBuilder route1(@Value("${route1.from}") String from, @Value("${route1.to}") String to
-			) {
+	public RouteBuilder direct(StringConverterProcessor processor) {
 		return new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
-				from(from).setHeader("to", constant(to)).to("direct:processing");
+				from("direct:demo").process(processor).toD("${header.to}");
 			}
 		};
-	}
 
-	@Bean public RouteBuilder direct(StringConverterProcessor processor) {
-		return new RouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				from("direct:processing").process(processor).toD("${header.to}");
-			}
-		};
-		
-	}
-	@Bean
-	public RouteBuilder route2(@Value("${route2.from}") String from, @Value("${route2.to}") String to) {
-		return new RouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				from(from).setHeader("to", constant(to)).to("direct:processing");
-			}
-		};
-	}
-	@Bean
-	public RouteBuilder route3() {
-		return new RouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				from("file:data2/in?noop=true").to("file:data2/out?fileName=copy-of-${file:name}");
-			}
-		};
 	}
 
 }
--- org.javacream.training.camel.spring/src/main/java/org/javacream/training/camel/web/SimpleRestController.java
+++ org.javacream.training.camel.spring/src/main/java/org/javacream/training/camel/web/SimpleRestController.java
@@ -2,7 +2,6 @@ package org.javacream.training.camel.web;
 
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,14 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleRestController {
 
 	@Autowired ProducerTemplate template;
-	@GetMapping(path = "/toStream")
-	public void toStream() {
-		template.sendBody("stream:out", "Hello");
-	}
 
 	@PostMapping(path = "/toFile")
-	public void toFile(@RequestBody String message, @RequestHeader("hugo") int number) {
-		template.sendBody("file:result", message);
+	public void toFile(@RequestBody String message, @RequestHeader("destination") String destination) {
+		template.sendBodyAndHeader("direct:demo", message, "to", destination);
 	}
 
 }
